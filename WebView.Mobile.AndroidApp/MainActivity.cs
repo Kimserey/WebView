bd958f1962cb802c1c7554159ccc91b17321f880
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-05-26 21:13:46 +0100

    Added android app and core mobile projects

---
+++ WebView.Mobile.AndroidApp/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WebView.Mobile.AndroidApp
+{
+    [Activity(Label = "WebView.Mobile.AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new WebView.Mobile.Core.WebView.App());
+        }
+    }
+}
+
---
+++ WebView.Mobile.Core/WebView.fs
@@ -0,0 +1,11 @@
+﻿namespace WebView.Mobile.Core
+
+open Xamarin.Forms
+
+module WebView =
+    
+    let content =
+        new ContentPage(Content = new Label(Text = "Hello world"))
+    
+    type App() = 
+        inherit Application(MainPage = content)
